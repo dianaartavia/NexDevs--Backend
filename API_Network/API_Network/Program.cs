5e48dbac4ce6f7e1b90ceeaa743f0608ba16bb2f
LuisCarlosSolis <168393621+LuisCarlosSolis@users.noreply.github.com>
2024-10-31 12:16:36 -0600

    feat: Agregado la url de la web hosteada online

--- API_Network/API_Network/Program.cs
+++ API_Network/API_Network/Program.cs
@@ -55,7 +55,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowSpecificOrigin",
         builder =>
         {
-            builder.WithOrigins("http://localhost:5173")
+            builder.WithOrigins("http://localhost:5173", "https://nex-devs-development.vercel.app")
                    .AllowAnyHeader()
                    .AllowAnyMethod();
         });
